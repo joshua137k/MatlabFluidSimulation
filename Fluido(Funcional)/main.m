62e6d97c32574609ac63fda0af68a7cde219fa0b
joshua137k <joshuacapistrano68@gmail.com>
2024-04-25 12:41:52 +0100

    super

--- Fluido(Funcional)/main.m
+++ Fluido(Funcional)/main.m
@@ -1,17 +1,19 @@
 close all
 clear all
 clc
-N=64;
+N=128;
 
-fluid = Fluid(N,3, 0, 0);
-
+fluid = Fluid(N,1.5, 0, 0);
+cube = Square([0.5*N,0.5*N+20],20,5,0);
+MatrixCube = 500*cube.draw(N); 
+fluid.setBlock(MatrixCube)
 maxIterations = 500;
 k=0;
 for t=0:maxIterations
 
-	cx = floor((0.5 * N));
+	cx = floor((0.5 * N))-10;
     cy = floor((0.5 * N));
-    
+
     for i =-1: 2
         for j = -1:2
             fluid.addDensity(cx + i, cy + j, floor(50+rand*100));
@@ -23,9 +25,13 @@ for t=0:maxIterations
         k = k + 0.01;
         fluid.addVelocity(cx, cy, v(1), v(2));
     end
-
+    cube.rotate(pi/3);
+    MatrixCube = 500*cube.draw(N); 
+    fluid.setBlock(MatrixCube)
 	fluid.step();
-    imagesc(fluid.density);
+    image = fluid.density+MatrixCube;
+    imagesc(image);
+    colormap(gray);
     drawnow
 
 end
